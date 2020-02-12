7848f114a9913feb36407fe4f70b0f12f4f586ef
Simon <54310162+siherrondemo@users.noreply.github.com>
2020-02-12 13:16:59 +0000

    Refreshed Broken Types & Omit LaunchDarkly Init
    Having reviewed the app properly, I have now fixed:
    - Broken and Unavailable Types for React.
    - Put back the client call for the Node SDK in index.ts
    - Modified the tsconfig.json to include additional moduleResolution/allowJS

--- index.ts
+++ index.ts
@@ -2,32 +2,34 @@ import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
 import { withLDProvider } from 'launchdarkly-react-client-sdk';
 import { withLDConsumer } from 'launchdarkly-react-client-sdk';
 
-console.log()
+const LaunchDarkly = require('launchdarkly-node-server-sdk');
+
 // TODO : Enter your LaunchDarkly SDK key here
-// const client = LaunchDarkly.init("sdk-b2f64606-70d8-4188-abea-410887091c61");
+const client = LaunchDarkly.init("sdk-6886d82f-4487-4073-8977-dd625dfbe55e");
 
-// const user = {
-//    "firstName":"Bob",
-//    "lastName":"Loblaw",
-//    "key":"louise@example.com",
-//    "custom":{
-//       "groups":"beta_testers"
-//    }
-// };
+//const client = LaunchDarkly.init("5dd325bdc3a8bb0968c8ab7f");
+const user = {
+   "firstName":"Bob",
+   "lastName":"Loblaw",
+   "key":"louise@example.com",
+   "custom":{
+      "groups":"beta_testers"
+   }
+};
 
-// client.once('ready', function() {
+client.once('ready', function() {
 //   // TODO : Enter the key for your feature flag here
-//   client.variation("show-widgets", user, false, function(err, showFeature) {
-//     // client.track("event-called", user);
-//     if (showFeature) {
-//       // application code to show the feature
-//       console.log("Showing your feature to " + user.key );
-//     } else {
-//       // the code to run if the feature is off
-//       console.log("Not showing your feature to " + user.key);
-//     }
-//     client.flush(function() {
-//       client.close();
-//     });
-//   });
-// });
+  client.variation("typescript", user, false, function(err, showFeature) {
+    // client.track("event-called", user);
+    if (showFeature) {
+      // application code to show the feature
+      console.log("Showing your feature to " + user.key );
+    } else {
+      // the code to run if the feature is off
+      console.log("Not showing your feature to " + user.key);
+    }
+    client.flush(function() {
+      client.close();
+    });
+  });
+});
